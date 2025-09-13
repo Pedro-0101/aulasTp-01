891914ebb31b44ba8d584db466f5b771619db8a2
Pedro-0101 <pedro.tiete@outlook.com>
2025-09-12 21:10:05 -0300

    aula 3

--- loja-tp1-angular/src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ loja-tp1-angular/src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,49 +1,28 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardProduto } from '../card-produto/card-produto';
 import { Produto } from '../../../model/produto';
+import { ProdutoService } from '../servicos/produto.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-lista-produtos',
   standalone: true,
   imports: [CommonModule, CardProduto],
   templateUrl: './lista-produtos.html',
-  styleUrls: ['./lista-produtos.css']
+  styleUrls: ['./lista-produtos.css'],
 })
 export class ListaProdutos {
-  // Catálogo estático (é possível fazer com signal)
-  produtos: Produto[] = [
-    {
-      id: 1,
-      nome: 'Teclado Mecânico',
-      preco: 1200,
-      descricao: 'Switch blue, RGB.',
-      imageUrl: 'images/keyboard.jpg',
-      promo: true,
-      state: 'Esgotado'
-    },
-    {
-      id: 2,
-      nome: 'Mouse Gamer 7200 DPI',
-      preco: 649.5,
-      descricao: '10 botões, macro programável.',
-      imageUrl: 'images/mouse.jpg',
-      state: 'Novo'
-    },
-    {
-      id: 3,
-      nome: 'Headset Surround 7.1',
-      preco: 899.9,
-      descricao: 'Microfone com redução de ruído.',
-      imageUrl: 'images/headset.jpg',
-      state: 'Usado'
-    }
-  ];
+  private produtoService = inject(ProdutoService);
+
+  private produtos = toSignal<Produto[], Produto[]>(this.produtoService.listar(), {
+    initialValue: [],
+  });
 
   // Filtro de promoção
   apenasPromo = signal(false);
   prodExibidos = computed(() =>
-    this.apenasPromo() ? this.produtos.filter((p) => p.promo) : this.produtos
+    this.apenasPromo() ? this.produtos().filter((p) => p.promo) : this.produtos()
   );
   alternarPromo() {
     this.apenasPromo.update((p) => !p);
